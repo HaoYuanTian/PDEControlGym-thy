c194424860fdddd82690d21d64a57876e389d2ca
Haoyuan Tian <2232147@mail.dhu.edu.cn>
2025-04-13 20:32:56 +0800

    注释

--- build/lib/pde_control_gym/src/rewards/__init__.py
+++ build/lib/pde_control_gym/src/rewards/__init__.py
@@ -4,3 +4,7 @@ from pde_control_gym.src.rewards.base_reward import BaseReward
 from pde_control_gym.src.rewards.ns_reward import NSReward
 
 __all__ = ["NormReward", "TunedReward1D", "BaseReward", "NSReward"]
+
+# 当使用from pde_control_gym.src.rewards import *
+# 会导入在__all__中列入的4个对象"NormReward", "TunedReward1D", "BaseReward", "NSReward"。
+# 方便打包管理。
