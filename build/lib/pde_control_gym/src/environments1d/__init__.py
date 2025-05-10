36c3b1f8e3e4e08cf45b8e7c029b92052e20686f
422lab <2222014@mail.dhu.edu.cn>
2025-05-10 16:18:25 +0800

    1

--- build/lib/pde_control_gym/src/environments1d/__init__.py
+++
@@ -1,4 +0,0 @@
-from pde_control_gym.src.environments1d.hyperbolic import TransportPDE1D
-from pde_control_gym.src.environments1d.parabolic import ReactionDiffusionPDE1D
-
-__all__ = ["TransportPDE1D", "ReactionDiffusionPDE1D"]
